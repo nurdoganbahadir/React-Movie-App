c9187f8444f17f612389bba6d3134293649a8bb6
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-18 20:32:02 +0300

    file created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Register from "./pages/Register";
+import AppRouter from "./router/AppRouter";
+
+const App = () => {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
