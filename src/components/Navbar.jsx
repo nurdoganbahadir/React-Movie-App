a2e647be7560b936767554cabd70adfebae9441a
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-19 22:52:48 +0300

    kullanıcı paneli update edildi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,30 +50,36 @@ export default function Navbar() {
                   transition
                   className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                 >
-                  <MenuItem>
-                    <Link
-                      to="/register"
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                    >
-                      Register
-                    </Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <Link
-                      to="/login"
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                    >
-                      Login
-                    </Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <span
-                      onClick={logOut}
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 cursor-pointer"
-                    >
-                      Log out
-                    </span>
-                  </MenuItem>
+                  {!currentUser && (
+                    <>
+                      <MenuItem>
+                        <Link
+                          to="/register"
+                          className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                        >
+                          Register
+                        </Link>
+                      </MenuItem>
+                      <MenuItem>
+                        <Link
+                          to="/login"
+                          className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                        >
+                          Login
+                        </Link>
+                      </MenuItem>
+                    </>
+                  )}
+                  {currentUser && (
+                    <MenuItem>
+                      <span
+                        onClick={logOut}
+                        className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 cursor-pointer"
+                      >
+                        Log out
+                      </span>
+                    </MenuItem>
+                  )}
                 </MenuItems>
               </Menu>
             </div>
